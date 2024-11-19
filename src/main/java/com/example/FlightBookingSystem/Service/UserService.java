d6b5efd3e3fef356bee9642db64133880e6eef7e
Harithaballa <harithaballa04@gmail.com>
2024-11-19 21:31:05 +0530

    add apis to user controller

--- src/main/java/com/example/FlightBookingSystem/Service/UserService.java
+++ src/main/java/com/example/FlightBookingSystem/Service/UserService.java
@@ -1,7 +1,22 @@
 package com.example.FlightBookingSystem.Service;
 
+import com.example.FlightBookingSystem.Controllers.UserController;
+import com.example.FlightBookingSystem.Model.User;
+import com.example.FlightBookingSystem.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public  UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User findById(long id) throws Exception {
+        return userRepository.findById(id).orElseThrow(()-> new Exception("user not found"));
+    }
 }
