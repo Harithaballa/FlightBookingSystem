804bedab7295461e1a9409d960a400bd87eae901
Harithaballa <harithaballa04@gmail.com>
2024-12-08 00:04:20 +0530

    exception handling of jwt exceptions

--- src/main/java/com/flightBookingSystem/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/flightBookingSystem/security/JwtAuthenticationEntryPoint.java
@@ -2,6 +2,10 @@ package com.flightBookingSystem.security;
 
 import java.io.IOException;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -17,7 +21,21 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
 
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied !!");
+        String message = "Unauthorized request";
+
+        if (authException.getCause() instanceof ExpiredJwtException) {
+            message = "Token has expired";
+        } else if (authException.getCause() instanceof MalformedJwtException) {
+            message = "Malformed JWT token";
+        } else if (authException.getCause() instanceof SignatureException) {
+            message = "Invalid JWT signature";
+        } else if (authException instanceof InsufficientAuthenticationException) {
+            message = "Authentication failed: Insufficient credentials";
+        }
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"error\": \"" + message + "\"}");
     }
 
 }
